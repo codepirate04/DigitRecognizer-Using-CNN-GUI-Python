1d3991c024dd811c4d3a5f13bbfd0e648b43ac9d
codepirate04 <44190927+codepirate04@users.noreply.github.com>
2020-09-06 15:15:24 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,22 +1,17 @@
 
 import tensorflow as tf
 
-# load data
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-# reshape to be (samples,width,height,channels)
 X_train = X_train.reshape((X_train.shape[0], 28, 28, 1)) 
 X_test = X_test.reshape((X_test.shape[0], 28, 28, 1))
 
-# Normalize inputs from 0-255 to 0-1
 X_train = X_train / 255
 X_test = X_test / 255
 
-# One hot encoding
 y_train = tf.keras.utils.to_categorical(y_train)
 y_test = tf.keras.utils.to_categorical(y_test)
 
-# Define a CNN model
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[28, 28, 1]))
 model.add(tf.keras.layers.MaxPooling2D(pool_size=2, strides=2))
@@ -24,13 +19,10 @@ model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(128, activation='relu'))
 model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
-# Compile model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Fit the model
 model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
 
-#Saving the model
 model.save('digitreg.h5')
 
 
